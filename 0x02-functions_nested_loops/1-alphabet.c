91a2ccf3bfcb1ed7ff4edd34e6fe44b30f65a12f
pearlmuslymarh <muslymarh21@gmail.com>
2022-07-06 14:00:52 -0700

    second push

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet in lowercase, followed by a newline
+ */
+void print_alphabet(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
