f9fd2d28b3d9e9eb34d7edcbc2bbdddab03d3301
pearlmuslymarh <muslymarh21@gmail.com>
2022-07-07 12:18:56 -0700

    betty

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+0. Multiple mains > this main file is a test for a postitive_or_negative()
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * positive_or_negative - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int positive_or_negative(char c)
+{
+return (write(1, &c, 1));
+}
