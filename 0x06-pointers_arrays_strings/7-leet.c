889caf5efffdf8378df192219dd4f2a9562ca7af
pearlmuslymarh <muslymarh21@gmail.com>
2022-07-15 05:51:43 -0700

    hot fix

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,8 +10,8 @@ char *leet(char *str)
 int indx1 = 0, indx2;
 char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 while (str[indx1])
-{	
-for (indx2 = 0; indx2 <= 7; indx2++)	
+{
+for (indx2 = 0; indx2 <= 7; indx2++)
 {
 if (str[indx1] == leet[indx2] ||
 str[indx1] - 32 == leet[indx2])
@@ -21,4 +21,3 @@ indx1++;
 }
 return (str);
 }
-
