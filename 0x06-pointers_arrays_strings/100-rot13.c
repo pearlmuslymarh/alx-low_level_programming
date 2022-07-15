188d7826969ba6cef56e80af633bb73d7abcf363
pearlmuslymarh <muslymarh21@gmail.com>
2022-07-15 05:56:52 -0700

    hot fix

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,7 +9,7 @@ char *rot13(char *str)
 {
 int indx1 = 0, indx2;
 char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-'G', 'H', 'I', 'J', 'K', 'L',	
+'G', 'H', 'I', 'J', 'K', 'L',
 'M', 'N', 'O', 'P', 'Q', 'R',
 'S', 'T', 'U', 'V', 'W', 'X',
 'Y', 'Z', 'a', 'b', 'c', 'd',
